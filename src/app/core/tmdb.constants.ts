988eb04df50d3e6baa5f6808fe4782f5086e55e0
Bayu Fajariyanto <bayuf08@gmail.com>
2023-11-12 19:37:07 +0700

    initial project

---
+++ src/app/core/tmdb.constants.ts
@@ -0,0 +1,3 @@
+export const TMDB_URL = 'https://api.themoviedb.org/3';
+export const TMDB_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmRlODM2YzhmMTQwYzY0YmI3ZDk4NDQ3ZmFkZTk5YSIsInN1YiI6IjVjYjUyMzNlYzNhMzY4NmFlYjgwYzQ0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.04WhMb44o8UqMEzWhH5m3AeC9s8stqhXGwS8_17yzp8';
+export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/w500';
